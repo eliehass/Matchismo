e18a875309585854484c6e50c5237f494d8d73ba
Elie Hassan <eliehass@mac.com>
2013-07-02 17:48:28 -0400

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Elie Hassan on 7/2/13.
+//  Copyright (c) 2013 Elie Hassan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
